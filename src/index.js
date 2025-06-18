91e7033a8c4594649f31cdc3d1f80efff50e365f
Hussnain Ahmad <mrhussnainahmad@gmail.com>
2025-06-18 06:29:47 +0500

    updated files

--- src/index.js
+++ src/index.js
@@ -18,7 +18,9 @@ app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: process.env.NODE_ENV === "production" 
+    ? ["https://talko-yourprivatechat.netlify.app"] 
+    : ["http://localhost:5173", "http://localhost:5174"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
